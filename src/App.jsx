f7c6f9ab9cdb76ec5c1f7956d5615d395aef62f5
ing-arriola <ingeniero.arriola@gmail.com>
2023-09-23 10:58:07 -0400

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-//import Register from './Register'
-import Login from './Login'
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout'
+import Register from './components/Register'
+import Login from './components/Login'
+import Home from './components/Home';
 
 
 function App() {
 
   return (
-    <main className="App">
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      {/* public routes */}
+      <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+
+          {/* we want to protect these routes */}
+          <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+    
   )
 }
 
